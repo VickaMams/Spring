79f28cc8e8e839855c9c91f1cac178eba1407dd0
Victoria2412 <nado122012@gmail.com>
2022-08-22 22:45:52 +0300

    spr_2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println((bean2.getMessage()));
+
+        Cat myCat = applicationContext.getBean("cat", Cat.class);
+        System.out.println(myCat.getMessage());
+        Cat myCat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(myCat2.getMessage());
+
+        System.out.println("Сравним bean объектов Helloworld по ссылке "+
+                (bean==bean2));
+        System.out.println("Сравним bean объектов Cat  по ссылке "+
+                (myCat==myCat2));
+
+
     }
 }
